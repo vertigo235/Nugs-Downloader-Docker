e82c4fb59e67c65540e405f1be66f3edbc6a0bf0
vertigo235 <jbjonesclt@gmail.com>
2023-10-31 16:49:46 -0400

    PUID and PGID optional

--- nugs-dl.sh
+++ nugs-dl.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 # Define default user and group IDs
-APP_USER_ID=${PUID:-1000}
-APP_GROUP_ID=${PGID:-1000}
+APP_USER_ID=${PUID:-}
+APP_GROUP_ID=${PGID:-}
 
-# Create a non-root user and group with customizable IDs
-groupadd -g $APP_GROUP_ID appgroup
-useradd -u $APP_USER_ID -g $APP_GROUP_ID -m appuser
+# Only create user and group if PUID and PGID are set
+if [ -n "$APP_USER_ID" ] && [ -n "$APP_GROUP_ID" ]; then
+    # Check if the group or GID already exists
+    if ! getent group appgroup > /dev/null && ! getent group $APP_GROUP_ID > /dev/null; then
+        groupadd -g $APP_GROUP_ID appgroup
+    fi
+
+    # Check if the user or UID already exists
+    if ! getent passwd appuser > /dev/null && ! getent passwd $APP_USER_ID > /dev/null; then
+        useradd -u $APP_USER_ID -g $APP_GROUP_ID -m appuser
+    fi
+
+    # Log the UID and GID that should be set
+    echo "Setting UID to: $APP_USER_ID, GID to: $APP_GROUP_ID"
+fi
 
 # Set default values for variables
-EMAIL=${EMAIL:-"youremail@email.com"}
-PASSWORD=${PASSWORD:-"your_password"}
-FORMAT=${FORMAT:-4}
-VIDEO_FORMAT=${VIDEO_FORMAT:-5}
-OUT_PATH=${OUT_PATH:-"/downloads"}
-TOKEN=${TOKEN:-}
-USE_FFMPEG_ENV_VAR=${USE_FFMPEG_ENV_VAR:-true}
+EMAIL=${NUGS_EMAIL:-""}
+PASSWORD=${NUGS_PASSWORD:-""}
+FORMAT=${NUGS_FORMAT:-4}
+VIDEO_FORMAT=${NUGS_VIDEO_FORMAT:-5}
+OUT_PATH=${NUGS_OUT_PATH:-"/downloads"}
+TOKEN=${NUGS_TOKEN:-}
+USE_FFMPEG_ENV_VAR=${NUGS_USE_FFMPEG_ENV_VAR:-true}
 JSON_FILE="/app/config.json"
 
 # Check if the config file already exists
@@ -32,10 +44,15 @@ if [ ! -f "$JSON_FILE" ]; then
     "useFfmpegEnvVar": $USE_FFMPEG_ENV_VAR
 }
 EOF
-  echo "Config file created: $JSON_FILE"
+  echo "nugs.net config file created: $JSON_FILE"
 else
-  echo "Config file already exists: $JSON_FILE"
+  echo "nugs.net config file already exists: $JSON_FILE"
 fi
 
-# Execute the application with optional arguments
-su appuser -c "/app/Nugs-DL $*"
+# Execute the application with optional arguments, conditionally based on PUID and PGID
+if [ -n "$APP_USER_ID" ] && [ -n "$APP_GROUP_ID" ]; then
+    su appuser -c "echo 'Executing as UID:' \$(id -u) ', GID:' \$(id -g); /app/Nugs-DL $*"
+else
+    echo 'Executing as current user'
+    /app/Nugs-DL $*
+fi
